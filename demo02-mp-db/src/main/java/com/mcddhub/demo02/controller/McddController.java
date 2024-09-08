dffeb71aae3bc9f79ec6832f70958ddcbbae6afa
coder-mcdd <mcdd1024@gmail.com>
2024-09-08 16:00:05 +0800

    :beers: feat(): mcdd crud
    #13-feature-add-db-module-and-docker-compose-file-to-auto-deploy

--- demo02-mp-db/src/main/java/com/mcddhub/demo02/controller/McddController.java
+++ demo02-mp-db/src/main/java/com/mcddhub/demo02/controller/McddController.java
@@ -1,8 +1,15 @@
 package com.mcddhub.demo02.controller;
 
+import cn.hutool.http.HttpStatus;
+import com.mcddhub.demo02.entity.Mcdd;
+import com.mcddhub.demo02.service.McddService;
+import jzxy.mcdd.common.entity.RestBean;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Objects;
 
 /**
  * McddController
@@ -13,8 +20,26 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @Slf4j
 @RestController
-@RequestMapping("/api/v1/mcdds")
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/mcdd/mcdds")
 public class McddController {
 
+    private final McddService service;
+
+
+    @GetMapping
+    RestBean<List<Mcdd>> list() {
+        List<Mcdd> list = service.list();
+        if (Objects.isNull(list)) {
+            return RestBean.failure(HttpStatus.HTTP_BAD_REQUEST, "No mcdd found");
+        }
+        return RestBean.success(list);
+    }
+
+    @PostMapping
+    RestBean<Mcdd> create(@RequestBody Mcdd mcdd) {
+        boolean save = service.save(mcdd);
+        return save ? RestBean.success(mcdd) : RestBean.failure(HttpStatus.HTTP_BAD_REQUEST, "mcdd save failed");
+    }
 
 }
---
+++ demo02-mp-db/src/main/java/com/mcddhub/demo02/controller/McddSeriesController.java
@@ -0,0 +1,42 @@
+package com.mcddhub.demo02.controller;
+
+import cn.hutool.http.HttpStatus;
+import com.mcddhub.demo02.entity.McddSeries;
+import com.mcddhub.demo02.service.McddSeriesService;
+import jzxy.mcdd.common.entity.RestBean;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * McddSeriesController
+ *
+ * @version 1.0.0
+ * @author: coder-mcdd
+ * @date: 2024/9/8 15:36
+ */
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/mcdd/series")
+public class McddSeriesController {
+    private final McddSeriesService service;
+
+    @GetMapping
+    RestBean<List<McddSeries>> list() {
+        List<McddSeries> list = service.list();
+        if (Objects.isNull(list)) {
+            return RestBean.failure(HttpStatus.HTTP_BAD_REQUEST, "No series found");
+        }
+        return RestBean.success(list);
+    }
+
+    @PostMapping
+    RestBean<McddSeries> create(@RequestBody McddSeries series) {
+        boolean save = service.save(series);
+        return save ? RestBean.success(series) : RestBean.failure(HttpStatus.HTTP_BAD_REQUEST, "Series save failed");
+    }
+}
