3ec74695f0204c532d4d3b263759a12a97d506b4
coder-mcdd <mcdd1024@gmail.com>
2024-09-08 20:11:53 +0800

    :beers: feat(demo02): test init
    #21

---
+++ demo03-test/src/main/java/com/mcddhub/demo03/Demo03Application.java
@@ -0,0 +1,20 @@
+package com.mcddhub.demo03;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Demo03Application
+ *
+ * @version 1.0.0
+ * @author: coder-mcdd
+ * @date: 2024/9/8 20:08
+ */
+@Slf4j
+@SpringBootApplication
+public class Demo03Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Demo03Application.class, args);
+    }
+}
